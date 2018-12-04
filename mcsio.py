f5866cfebed48c6b63eb4124d0f6fe57f1ff6280
wenweii <weiwei86122538@gmail.com>
2018-12-04 19:45:20 +0800

    1204

--- mcsio.py
+++ mcsio.py
@@ -24,25 +24,28 @@ import time
 import httplib, urllib
 import json
 import Adafruit_DHT
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 deviceId = "DSld0qjo"
 deviceKey = "S8k6nWJjynPcVMX6"
 def post_to_mcs(payload):
-	headers = {"Content-type": "application/json", "deviceKey": deviceKey}
-	not_connected = 1
-	while (not_connected):
-		try:
-			conn = httplib.HTTPConnection("api.mediatek.com:80")
-			conn.connect()
-			not_connected = 0
-		except (httplib.HTTPException, socket.error) as ex:
-			print ("Error: %s" % ex)
-			time.sleep(10)
-			 # sleep 10 seconds
-	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers)
-	response = conn.getresponse()
-	print( response.status, response.reason, json.dumps(payload), time.strftime("%c"))
-	data = response.read()
-	conn.close()
+ headers = {"Content-type": "application/json", "deviceKey": deviceKey}
+ not_connected = 1
+ while (not_connected):
+  try:
+   conn = httplib.HTTPConnection("api.mediatek.com:80")
+   conn.connect()
+   not_connected = 0
+  except (httplib.HTTPException, socket.error) as ex:
+   print ("Error: %s" % ex)
+   time.sleep(10)
+    # sleep 10 seconds
+ conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers)
+ response = conn.getresponse()
+ print( response.status, response.reason, json.dumps(payload), time.strftime("%c"))
+ data = response.read()
+ conn.close()
 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
@@ -70,18 +73,16 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 
 while True:
-	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
-	if h0 is not None and t0 is not None:
-		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
+ SwitchStatus = GPIO.input(24)
+ h0, t0= Adafruit_DHT.read_retry(sensor, pin)
+ if h0 is not None and t0 is not None:
+  print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
 
-		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
-		{"dataChnId":"Temperature","values":{"value":t0}}]}
-		post_to_mcs(payload)
-		time.sleep(10)
+  payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+  {"dataChnId":"Temperature","values":{"value":t0}},{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}
+  post_to_mcs(payload)
+  time.sleep(10)
 
-	else:
-		print('Failed to get reading. Try again!')
-		sys.exit(1)
-
-
-
+ else:
+  print('Failed to get reading. Try again!')
+  sys.exit(1)
